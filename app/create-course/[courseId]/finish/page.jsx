70e676a7f22080a982ef06e9c60651bbde310ff7
Musyy002 <Muskanbandri1991@gmail.com>
2025-02-14 19:55:04 +0530

    Files Updated

--- app/create-course/[courseId]/finish/page.jsx
+++ app/create-course/[courseId]/finish/page.jsx
@@ -53,8 +53,9 @@ function FinishScreen() {
 
         <CourseBasicInfo course={course} refreshData={()=>console.log} />
         <h2 className='mt-3'>Course URL:</h2>
-        <h2 className='text-center text-gray-400 border p-2 round flex gap-5 items-center'>{process.env.NEXT_PUBLIC_VERCEL_HOST_NAME}/course/{course?.courseId}
-        <HiOutlineClipboardDocumentCheck className='h-5 w-5 cursor-pointer' onClick={async()=>await navigator.clipboard.writeText(process.env.NEXT_PUBLIC_VERCEL_HOST_NAME+"/course/"+course?.courseId)} /></h2>
+        <h2 className='text-center text-gray-400 border p-2 round flex gap-5 items-center'>{process.env.NEXT_PUBLIC_VERCEL_HOST_NAME || 'ENV VARIABLE NOT LOADED'}/course/{course?.courseId}
+        <HiOutlineClipboardDocumentCheck className='h-5 w-5 cursor-pointer' onClick={async()=>await navigator.clipboard.writeText((process.env.NEXT_PUBLIC_VERCEL_HOST_NAME || 'ENV MISSING') + "/course/" + course?.courseId
+    )} /></h2>
 
     </div>
   )
--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { UserButton } from '@clerk/nextjs'
 
 function Header() {
   return (
     <div className='flex justify-between items-center p-5 shadow-sm'>
+    <Link href="/dashboard">
     <Image src={'favicon.svg'} width={30} height={30} alt='image not available'/>
+    </Link>
     <UserButton/>
     </div>
   )
